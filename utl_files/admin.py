0a5f3bffb3c97fa60c5963af732b946392db3a51
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-06-22 17:05:13 -0400

    Add all models to admin site

--- utl_files/admin.py
+++ utl_files/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Application, TNSite, Package, UTLFile
+
+admin.site.register(Application)
+admin.site.register(TNSite)
+admin.site.register(Package)
+admin.site.register(UTLFile)
