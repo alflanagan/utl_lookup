cc8584166c20a4d15ce9c43106b5c96ec8bbb3e5
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-07-17 16:28:52 -0400

    Created index view from bootstrap template

--- papers/views.py
+++ papers/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import NewsPaper, TNSite
 
 
 def index(request):
-    return HttpResponse("Hello, world. This is the index page of the app, where you'll"
-                        " choose a web site to work with, and have various search options.")
+    context = {"papers": NewsPaper.objects.all()}
+    return render(request, 'papers/index.html', context=context)
 
