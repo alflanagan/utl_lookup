28ef31b32915dc88b5bc3f8f019ae9180402a2b8
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-10-01 17:40:48 -0400

    sourceable file to set environment vars

---
+++ env.sh
@@ -0,0 +1,4 @@
+# set up shell variables. must be sourced from command line
+export PYTHONPATH=/home/aflanagan/Devel/utl_lookup:/home/aflanagan/Devel/utl_indexer:/home/aflanagan/Devel/shared_libraries
+export DJANGO_SITENAME=utl_lookup
+export DJANGO_SETTINGS_MODULE=${DJANGO_SITENAME}.settings
