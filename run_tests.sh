709cad934933b14457516938aab188f9e22081b1
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-10-22 13:03:03 -0400

    Add --cover option

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env dash
 # run tests with warnings enabled, except specific ones from library modules
-python -Wall -Wignore:::django.dispatch.dispatcher:99 -Wignore:::django.template.base:1189 -Wignore:::django.utils.six:80 manage.py test $*
 
+if [ "$1" != "--cover" ]; then
+    python -Wall -Wignore:::django.dispatch.dispatcher:99 -Wignore:::django.template.base:1189 -Wignore:::django.utils.six:80 manage.py test $*
+else
+    coverage run --source='.' --omit='*/migrations/*','utl_lookup/dev-settings.py','utl_files/test_models.py','*/admin.py','utl_lookup/wsgi.py' manage.py test
+    coverage report --omit='*/__init__.py'
+    coverage report -m utl_files/models.py
+fi
