e91d1ea455e86fafc85ccd97bd66927ce93b440f
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-10-22 13:01:14 -0400

    Add tests for utl_files.UTLFile

--- utl_files/test_models.py
+++ utl_files/test_models.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import os
+
 from django.test import TestCase, TransactionTestCase
 from django.db.utils import DataError
 from django.core.exceptions import ValidationError
@@ -6,14 +11,23 @@ from .models import Application, MacroDefinition, MacroRef, Package, PackageDep,
 from .models import PackageError, UTLFile, UTLFileError
 
 
-class TestApplication(TestCase):
+class ApplicationTestCase(TestCase):
+    """Unit tests for :py:class:`utl_files.models.Application`.
 
+    There's not really anything to test here, but I like to validate basic assumptions so that
+    if they change, I'm forced to update tests to match.
+
+    """
     def setUp(self):
         """Create a simple Application record for testing."""
         app = Application(name="editorial")
         app.save()
 
     def test_insert(self):
+        """Unit test for :py:meth:`utl_files.models.Application` and
+        :py:meth:`utl_files.models.Application.save`.
+
+        """
         self.assertEqual(Application.objects.count(), 1)
         the_app = Application.objects.all()[0]
         self.assertEqual(the_app.name, "editorial")
@@ -33,7 +47,7 @@ class TestApplication(TestCase):
         self.assertRaises(ValidationError, bad_app.full_clean)
 
 
-class TestPackage(TransactionTestCase):
+class PackageTestCase(TransactionTestCase):
     """Unit tests for :py:class:`utl_files.models.Package`."""
 
     TEST_APP = "editorial"
@@ -63,7 +77,8 @@ class TestPackage(TransactionTestCase):
         pkg = Package.objects.get(name=self.TEST_NAME, version=self.TEST_VERSION)
         pkg.delete()
         self.assertEqual(Package.objects.count(), 0)
-        self.assertRaises(Package.DoesNotExist, Package.objects.get, name=self.TEST_NAME, version=self.TEST_VERSION)
+        self.assertRaises(Package.DoesNotExist, Package.objects.get,
+                          name=self.TEST_NAME, version=self.TEST_VERSION)
 
     def test_bad(self):
         app = Application.objects.first()
@@ -80,3 +95,66 @@ class TestPackage(TransactionTestCase):
                       app=app)
         self.assertRaises(DataError, pkg.save)
         self.assertRaises(ValidationError, pkg.full_clean)
+
+
+class UTLFileTestCase(TransactionTestCase):
+
+    TEST_APP = "editorial"
+    TEST_PKG = "skin-editorial-core-base"
+    TEST_VERSION = "1.45.1.0"
+    PKG_DIRECTORY = "utl_files/test_data/skin-editorial-core-base"
+    SIMPLE_UTL_FILE = "includes/footer-spotless.inc.utl"
+    MACROS_FILE = "includes/macros.inc.utl"
+
+    def setUp(self):
+        self.app = Application(name=self.TEST_APP)
+        self.app.save()
+        self.pkg = Package(name=self.TEST_PKG,
+                           version=self.TEST_VERSION,
+                           is_certified=True,
+                           app=self.app)
+        self.pkg.save()
+        # one of the simplest UTL files in the test data
+        self.simple_utl = UTLFile(file_path=self.SIMPLE_UTL_FILE,
+                                  pkg_directory=self.PKG_DIRECTORY,
+                                  pkg=self.pkg)
+        self.simple_utl.save()
+        self.macros_utl = UTLFile(file_path=self.MACROS_FILE,
+                                  pkg_directory=self.PKG_DIRECTORY,
+                                  pkg=self.pkg)
+        self.macros_utl.save()
+
+    def test_create(self):
+        self.assertEqual(UTLFile.objects.count(), 2)
+        thefile = UTLFile.objects.get(file_path=self.SIMPLE_UTL_FILE)
+        self.assertEqual(thefile.file_path, self.SIMPLE_UTL_FILE)
+        self.assertEqual(thefile.pkg_directory, self.PKG_DIRECTORY)
+        self.assertEqual(thefile.pkg, self.pkg)
+
+    def test_full_file_path(self):
+        self.assertEqual(str(self.simple_utl.full_file_path),
+                         os.path.join(self.PKG_DIRECTORY, self.SIMPLE_UTL_FILE))
+
+    def test_to_dict(self):
+        self.assertDictEqual(self.simple_utl.to_dict(),
+                             {'name': self.TEST_PKG,
+                              'path': self.SIMPLE_UTL_FILE,
+                              'pkg_directory': self.PKG_DIRECTORY,
+                              'version': self.TEST_VERSION})
+
+    def test_str(self):
+        self.assertEqual(str(self.simple_utl), "{}/{}:{}".format(self.TEST_APP, self.TEST_PKG,
+                                                                 self.SIMPLE_UTL_FILE))
+
+    def test_get_macros_none(self):
+        """Test that :py:meth:`utl_file.models.UTLFile.get_macros` can successfully do nothing."""
+        self.simple_utl.get_macros()
+        self.assertEqual(MacroDefinition.objects.count(), 0)
+        self.assertEqual(MacroRef.objects.count(), 0)
+
+    def test_get_macros_many(self):
+        """Test that :py:meth:`utl_file.models.UTLFile.get_macros` can successfully load macros."""
+        self.macros_utl.get_macros()
+        self.assertEqual(MacroDefinition.objects.count(), 11)
+        self.assertEqual(MacroRef.objects.count(), 14)
+
