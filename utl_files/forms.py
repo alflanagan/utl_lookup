a147151f495fc13c79ad0ef0f8d5a53697d47589
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-03-22 12:07:17 -0400

    Fix class reference to TNSite.objects; caused db setup to fail

--- utl_files/forms.py
+++ utl_files/forms.py
@@ -11,12 +11,22 @@
 
 """
 from django import forms
-from papers.models import TNSite
+from papers.models import TownnewsSite
+
+# pylint: disable=no-member
 
 
 class XrefContextForm(forms.Form):
     """A form used to set context (site, global skin, skin) for the UTL cross-reference page."""
-    site = forms.ChoiceField(widget=forms.Select(attrs={"class": "form-control"}),
-                             choices=[(x.name, x.name) for x in TNSite.objects.all()])
+    site = forms.ChoiceField(widget=forms.Select(attrs={"class": "form-control"}))
     global_skin = forms.ChoiceField(choices=[])
     app_skin = forms.ChoiceField(choices=[])
+
+    # pylint: disable=too-many-arguments
+    def __init__(self, data=None, files=None, auto_id='id_%s', prefix=None,
+                 initial=None, label_suffix=None,
+                 empty_permitted=False, field_order=None):
+        super().__init__(data, files, auto_id, prefix, initial, label_suffix=label_suffix,
+                         empty_permitted=empty_permitted, field_order=field_order)
+        # this needs to be init() since it will fail if database tables not created yet
+        self.site.choices = [(x.name, x.name) for x in TownnewsSite.objects.all()]
