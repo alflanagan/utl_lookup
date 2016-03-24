7c5872d2596b450690ce7546b8452799730990b2
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-03-24 17:46:24 -0400

    Add docs to /admin

--- utl_lookup/urls.py
+++ utl_lookup/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^papers/', include('papers.urls')),
     url(r'^files/', include('utl_files.urls')),
