79c808af8ff5ef567eb3062813199383bb374fb9
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-03-28 11:23:18 -0400

    Add $.getJSON call to populate global skins

--- utl_files/static/js/utl_files.js
+++ utl_files/static/js/utl_files.js
@@ -8,10 +8,20 @@
 
 
 $(function() {
-
-  $("#id_site").on("change", function(evt) {
-    var selected_site = evt.target.value;
-    $.ajax("utl_files/global_skins_for_site?site=" + selected_site);
-  });
-
+    console.log("setting selectedIndex to -1");
+    $("#id_site").prop("selectedIndex") = -1;
+    $("#id_site").on("change", function(evt) {
+        var selected_site = evt.target.value;
+        $("#id_global_skin option").detach();
+        $.getJSON("api/global_skins_for_site/" + selected_site + "/",
+            function(data) {
+                var new_option;
+                $("#id_global_skin").append('<option>Select One:</option>');
+                data.forEach(function(datum) {
+                    new_option = $('<option value="' + encodeURIComponent(datum) + '">' + datum + '</option>');
+                    $("#id_global_skin").append(new_option);
+                });
+            }
+        );
+    });
 });
