7db801479b2a87a2c9ba8faab4cd78af808d162b
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-03-28 12:42:54 -0400

    Get demo/ page search bar working

--- utl_files/static/js/utl_files.js
+++ utl_files/static/js/utl_files.js
@@ -8,18 +8,26 @@
 
 
 $(function() {
-    console.log("setting selectedIndex to -1");
-    $("#id_site").prop("selectedIndex") = -1;
-    $("#id_site").on("change", function(evt) {
-        var selected_site = evt.target.value;
-        $("#id_global_skin option").detach();
-        $.getJSON("api/global_skins_for_site/" + selected_site + "/",
+    $("#div_id_site ul.dropdown-menu li").on("click", function(evt) {
+        var the_site = evt.target.textContent;
+        $("#selected_site").value = the_site;
+        $("#id_global_skin li").detach();
+        $.getJSON("api/global_skins_for_site/" + the_site + "/",
             function(data) {
                 var new_option;
-                $("#id_global_skin").append('<option>Select One:</option>');
                 data.forEach(function(datum) {
-                    new_option = $('<option value="' + encodeURIComponent(datum) + '">' + datum + '</option>');
+                    new_option = $('<li>' + datum + '</li>');
                     $("#id_global_skin").append(new_option);
+                });
+            }
+        );
+        $("#id_app_skin li").detach();
+        $.getJSON("api/app_skins_for_site/" + the_site + "/",
+            function(data) {
+                var new_option;
+                data.forEach(function(datum) {
+                    new_option = $('<li>' + datum + '</li>');
+                    $("#id_app_skin").append(new_option);
                 });
             }
         );
