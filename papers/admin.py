9aa03e1840a8e19febd1dee240294b293acc01dd
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-04-01 18:30:16 -0400

    Add URL to site admin list

--- papers/admin.py
+++ papers/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import TownnewsSite, NewsPaper
 
+class TownnewsSiteAdmin(admin.ModelAdmin):
+    list_display = ("URL", "name")
 
-admin.site.register(TownnewsSite)
+admin.site.register(TownnewsSite, TownnewsSiteAdmin)
 admin.site.register(NewsPaper)
