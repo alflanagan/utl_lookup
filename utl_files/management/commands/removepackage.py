27b43d34c96f2cb36c346f6e256eaa2fb281a173
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-04-04 17:41:01 -0400

    Add site param to properly remove custom packages

--- utl_files/management/commands/removepackage.py
+++ utl_files/management/commands/removepackage.py
@@ -1,21 +1,33 @@
 # -*- coding:utf-8 -*-
-from pathlib import Path
+"""Convenient manage.py command to remove a package from the database."""
 from django.core.management.base import BaseCommand, CommandError
-from utl_files.models import Package, PackageError
+from utl_files.models import Package, TownnewsSite
 
+# pylint:disable=no-member
 
 class Command(BaseCommand):
-    help = 'Lists all Townnews packages currently known. Warning: list may be long.'
+    """Command 'manage.py removepackage'."""
+    help = 'Removes a stored Townnews package.'
 
     def add_arguments(self, parser):
+        parser.add_argument('package-site', help='Site URL', metavar='PKG-SITE')
         parser.add_argument('package-name', help='Townnews package name', metavar='PKG-NAME')
         parser.add_argument('package-ver', help='package version number', metavar='PKG-VERSION')
 
     def handle(self, *args, **options):
         try:
-            pkg = Package.objects.get(name=options['package-name'], version=options['package-ver'])
+            siteurl = options['package-site']
+            siteurl = siteurl if siteurl.startswith('http:') else 'http://' + siteurl
+            try:
+                site = TownnewsSite.objects.get(URL=siteurl)
+            except TownnewsSite.DoesNotExist:
+                raise CommandError("Can't find site {}".format(siteurl))
+            pkg = Package.objects.get(name=options['package-name'],
+                                      version=options['package-ver'],
+                                      site=site)
             pkg.delete()
         except Package.DoesNotExist:
-            raise CommandError("Can't remove package '{}' ({}) as it is not present "
-                               "(try manage.py listpackages.)".format(options['package-name'],
-                                                                   options['package-ver']))
+            raise CommandError("Can't remove package '{}:{}' ({}) as it is not present "
+                               "(try manage.py listpackages.)".format(options['package-site'],
+                                                                      options['package-name'],
+                                                                      options['package-ver']))
