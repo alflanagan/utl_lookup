279c59573995d593df93985b36c8fe1bf80e0a2d
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-04-08 12:06:51 -0400

    Consolidate tests, reduce overhead

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env dash
 cd /mnt/extra/Devel/utl_lookup
-# coverage will find papers/managment/commands with --source=papers unless
-# there's an --omit param, in which case you have specify it separately
-coverage run --source=papers,papers/management/commands --omit='papers/migrations/*','*/test_*','*/__init__.py','*/urls.py' --branch manage.py test papers
-coverage report -m
-coverage erase
-# and yet... I don't have to specify utl_files/management/commands. ?!?!
-coverage run --source=utl_files --omit='utl_files/migrations/*','*/__init__.py','*/test_*','*/urls.py' --branch manage.py test utl_files
+coverage run --source=papers,utl_files --omit='*/migrations/*','*/test_*','*/__init__.py','*/urls.py' --branch manage.py test
 coverage report -m
 coverage erase
