cec229783ce22936f486b37b51a9c93114988b76
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-04-08 17:55:42 -0400

    fix weird issue finding tests

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env dash
 cd /mnt/extra/Devel/utl_lookup
-coverage run --source=papers,utl_files --omit='*/migrations/*','*/test_*','*/__init__.py','*/urls.py' --branch manage.py test
+coverage run --source=papers,utl_files --omit='*/migrations/*','*/test_*','*/__init__.py','*/urls.py' --branch manage.py test papers utl_files
 coverage report -m
 coverage erase
