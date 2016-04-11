7641c6e658dfc10d081da4f97f2a9226858e57bd
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-04-11 18:42:02 -0400

    remove a lot of superfluous semicolons

--- utl_files/static/js/utl_files.js
+++ utl_files/static/js/utl_files.js
@@ -12,7 +12,7 @@
       asi: true
 */
 
-$(function() {
+$(function () {
     "use strict";
 
     /**
@@ -21,10 +21,10 @@ $(function() {
      * @param {Object} evt The object describing the event which we
      * are responding to.
      */
-    const global_skin_onclick = function(evt) {
-        const new_text = evt.target.textContent;
-        $("#id_global_skin_label").prop("innerText", new_text);
-    };
+    const global_skin_onclick = function (evt) {
+        const new_text = evt.target.textContent
+        $("#id_global_skin_label").prop("innerText", new_text)
+    }
 
     /**
      * Click event handler for app_skin selection control.
@@ -32,13 +32,13 @@ $(function() {
      * @param {Object} evt The object describing the event which we
      * are responding to.
      */
-    const app_skin_onclick = function(evt) {
-        const new_text = evt.target.textContent;
-        $("#id_app_skin_label").prop("innerText", new_text);
-    };
+    const app_skin_onclick = function (evt) {
+        const new_text = evt.target.textContent
+        $("#id_app_skin_label").prop("innerText", new_text)
+    }
 
 
-    $("#tree-view").jstree();
+    $("#tree-view").jstree()
 
     /**
      * Enable a control if there is data in an array, disable otherwise.
@@ -47,13 +47,13 @@ $(function() {
      * @param {Object} control The control(s) to be enabled, a JQuery
      * selector.
      */
-    let enable_if_data = function(data, control) {
+    let enable_if_data = function (data, control) {
         if (data.length > 0) {
-            control.removeAttr("disabled");
+            control.removeAttr("disabled")
         } else {
-            control.attr("disabled", "");
+            control.attr("disabled", "")
         }
-    };
+    }
 
 
     /**
@@ -67,57 +67,57 @@ $(function() {
      * @param {function} click_handler Optional handler for the
      *     created elements' onclick event.
      */
-    const add_li_from_data = function(data, parent, click_handler) {
-        data.forEach(function(datum) {
-            const new_elem = $('<li>' + datum + '</li>');
+    const add_li_from_data = function (data, parent, click_handler) {
+        data.forEach(function (datum) {
+            const new_elem = $('<li>' + datum + '</li>')
             if (click_handler !== undefined) {
-                new_elem.on("click", click_handler);
-            };
-            parent.append(new_elem);
-        });
+                new_elem.on("click", click_handler)
+            }
+            parent.append(new_elem)
+        })
         if (data.length === 1) {
-            parent.children("li").click();
+            parent.children("li").click()
         }
-    };
+    }
 
     // note our dropdown menus have form
     //  <div id="div_id_MODEL">
     //    <button id="id_MODEL_label"></button>
     //    <ul id="id_MODEL">...
-    $("#id_site li").on("click", function(evt) {
+    $("#id_site li").on("click", function (evt) {
         const the_site = evt.target.textContent,
-            the_label = $("#id_site_label")[0];
+            the_label = $("#id_site_label")[0]
 
-        the_label.innerText = the_site;
+        the_label.innerText = the_site
 
-        $("#id_global_skin li").detach();
-        $("#id_global_skin_label").html("Global Skin<span class=\"caret\">");
-        $("#id_app_skin li").detach();
-        $("#id_app_skin_label").html("App Skin<span class=\"caret\">");
+        $("#id_global_skin li").detach()
+        $("#id_global_skin_label").html("Global Skin<span class=\"caret\">")
+        $("#id_app_skin li").detach()
+        $("#id_app_skin_label").html("App Skin<span class=\"caret\">")
 
         $.getJSON("api/global_skins_for_site/" + the_site + "/").done(
-            function(data) {
-                enable_if_data(data, $("#id_global_skin_label"));
-                add_li_from_data(data, $("#id_global_skin"), global_skin_onclick);
+            function (data) {
+                enable_if_data(data, $("#id_global_skin_label"))
+                add_li_from_data(data, $("#id_global_skin"), global_skin_onclick)
             }
-        ).fail(function() {
-            console.log("ERROR in api call to global_skins_for_site.");
+        ).fail(function () {
+            console.log("ERROR in api call to global_skins_for_site.")
             arguments.forEach(
-                function(arg) {
+                function (arg) {
                     console.log(arg)
                 })
-        });
+        })
 
         $.getJSON("api/app_skins_for_site/" + the_site + "/").done(
-            function(data) {
-                enable_if_data(data, $("#id_app_skin_label"));
-                add_li_from_data(data, $("#id_app_skin"), app_skin_onclick);
+            function (data) {
+                enable_if_data(data, $("#id_app_skin_label"))
+                add_li_from_data(data, $("#id_app_skin"), app_skin_onclick)
             }
-        ).fail(function() {
-            console.log("ERROR in api call to app_skins_for_site.");
+        ).fail(function () {
+            console.log("ERROR in api call to app_skins_for_site.")
             for (let i = 0; i < arguments.length; i++) {
-                console.log(arguments[i]);
+                console.log(arguments[i])
             }
-        });
-    });
-});
+        })
+    })
+})
