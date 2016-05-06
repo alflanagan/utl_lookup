47a2779f2f85b80b0152f6ec44c1507704b63249
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-05-06 11:36:00 -0400

    Replace code with refs to utl_files_common routines.

--- utl_files/static/js/utl_files_files.js
+++ utl_files/static/js/utl_files_files.js
@@ -28,10 +28,17 @@
       varstmt: true
 */
 /* global
-   $ */
+   $, utl_files
+ */
 
 $(function () {
   "use strict";
+
+  if (window.utl_files === undefined) {
+    throw Error("Attempted to use utl_files_files module without loading util_files module.")
+  }
+
+  let utl_files = window.utl_files
 
   // ID selector strings, use constant to avoid mistyping
   const TREE_VIEW = "#tree-view",
@@ -49,9 +56,9 @@ $(function () {
     $(FILES_PNL).hide()
     $("#files-tab").removeClass("active")
     let the_id = evt.target.id
-    if (! the_id.endsWith("-tab")) {
+    if (!the_id.endsWith("-tab")) {
       the_id = evt.target.parentNode.id
-}
+    }
     switch (the_id) {
       case "files-tab":
         $(FILES_PNL).show()
@@ -70,173 +77,6 @@ $(function () {
   $("#defs-tab").on("click", on_tab_click)
   $("#refs-tab").on("click", on_tab_click)
 
-  /**
-   * @summary An object that represents a bootstrap dropdown control
-   * on the page.
-   *
-   * @description A bootstrap control is made up of a &lt;div&gt;
-   * containing a &lt;button&gt; and an &lt;ul&gt; list. This object
-   * handles the initial appearance, click handling, and updating the
-   * button text with the value of the selected item.
-   *
-   * @constructor
-   * @global
-   *
-   * @param {string} ul_id The id attribute of the &lt;ul&gt; element used
-   * for dropdown
-   * @param {string} label_id The id attribute of the &lt;button&gt;
-   * element acting as a label/trigger
-   * @param {string} label_text The text to show on the control when
-   * no item is selected
-   * @param {Function} select_handler Function called when a
-   * drop-down item is selected. Gets this object as an argument
-   *
-   */
-  const DropDownControl = function (ul_id, label_id, label_text, select_handler) {
-      this.label_id = label_id
-      this.ul_id = ul_id
-      this.label_text = label_text
-      this.picked = false
-      this.handler = select_handler
-      console.log('Creating DropDownControl("' + ul_id + '", "' + label_id + "...")
-
-      /**
-       * @summary Event handler for click on list item.
-       *
-       * @description An event handler triggered when one of the
-       * dropdown items is clicked. Sets label to text of selected
-       * item, sets <var>picked</var> to <code>true</code>, then calls
-       * user-supplied handler, if any.
-       *
-       * @param {Object} evt the Event object.
-       *
-       * @param {string} evt.target The list item that was clicked.
-       */
-      this.onclick = evt => {
-        this.text(evt.target.textContent)
-        this.picked = true
-        this.handler(this)
-      }
-
-      // ensure existing list items have click handler
-      $(ul_id + " li").on("click", this.onclick)
-
-      /**
-       * Get or set the label text.
-       *
-       * @param {string} arg Set the label text for this control to
-       * the value of <var>arg</var> (plus a caret decoration). If
-       * <var>arg</var> is <code>undefined</code>, the text is
-       * unchanged (but still returned).
-       *
-       * @return {string} The text of the currently selected item
-       */
-      this.text = arg => {
-        if (arg === undefined) {
-          return $(this.label_id).prop("innerText")
-        } else {
-          this.label_text = arg
-          $(this.label_id).html(this.label_text + "<span class=\"caret\"></span>")
-          return arg
-        }
-      }
-
-      /**
-       * Disable the control
-       */
-      this.disable = () => {
-        $(this.label_id).attr("disabled", "")
-      }
-
-      /**
-       * Enable the control
-       */
-      this.enable = () => {
-        $(this.label_id).removeAttr("disabled")
-      }
-
-      /**
-       * @summary Fills dropdown with string values
-       *
-       * @description Clears the current list, then creates a list
-       * item for each element in <var>data</var>. Clicking the
-       * element will trigger this object's <code>onclick()</code>
-       * handler. If there are no items in <var>data</var>, disables
-       * the control. If there is exactly 1 item in <var>data</var>,
-       * automatically selects it (triggering click event).
-       *
-       * @param {Array} data A list of items to be added.
-       */
-      this.add_li_from_data = data => {
-          $(this.ul_id + " li").detach()
-            // don't prevent user from filling tree if no options or
-            // just one
-          this.picked = (data.length < 2)
-          data.forEach(
-            datum => {
-              const new_elem = $('<li>' + datum + '</li>')
-              new_elem.on("click", this.onclick)
-              $(this.ul_id).append(new_elem)
-            })
-          switch (data.length) {
-            case 0:
-              this.disable()
-              break;
-            case 1:
-              this.disable()
-                // automatically select for user
-              $(this.ul_id).children("li").click()
-              break;
-            default:
-              this.enable()
-          }
-        } //add_li_from_data
-
-      /**
-       * Resets dropdown to empty, disabled state
-       *
-       * @param {string} new_label If present, text displayed on
-       * button is changed to this value
-       */
-      this.reset = (new_label) => {
-        $(this.ul_id + " li").detach()
-        if (new_label !== undefined) {
-          this.label_text = new_label
-        }
-        this.text(this.label_text)
-        this.disable()
-        this.picked = false
-      }
-
-      /**
-       * @summary Fill the drop-down with results of an API call.
-       *
-       * IMPORTANT: caller should reset control first
-       *
-       * @param {string} api_name The API name (the fixed part of the
-       * URL that follows 'api/')
-       *
-       * @param {string} site_name The name of the site selected
-       *
-       */
-      this.fill_from_api = (api_name, site_name) => {
-          $.getJSON("api/" + api_name + "/" + site_name + "/")
-            .done(data => {
-              // console.log(data)
-              this.add_li_from_data(data)
-            })
-            .fail(() => {
-              console.log("ERROR in api call to " + api_name + "/" + site_name + ".")
-              arguments.forEach(
-                function (arg) {
-                  console.log(arg)
-                })
-            })
-        } // fill_from_api()
-
-      return this;
-
-    } // DropDownControl
 
   /**
    * @description An object to manage the list of files in a tab on
@@ -258,7 +98,7 @@ $(function () {
       core: {
         check_callback: true,
         multiple: false,
-        error: handle_jstree_error,
+        error: utl_files.handle_jstree_error,
         themes: {
           icons: true,
           responsive: true
@@ -307,14 +147,14 @@ $(function () {
       this.root_node = this.jst.get_node(this.root_id, true)
     }
     return this
-  }
+  } // FilesView()
 
   /**
-   * @summary A node in the Tree View listing packages.
+   * A node in the Tree View listing packages.
    *
-   * @description An object that represents a node in the Tree
-   * View. Depending on the selections made in the search area, a node
-   * may have children which are the names of packages.
+   * An object that represents a node in the Tree View. Depending on
+   * the selections made in the search area, a node may have children
+   * which are the names of packages.
    *
    * @constructor
    * @global
@@ -322,11 +162,11 @@ $(function () {
    * @param {string} list_id the ID of the HTML element that acts as a
    * root node
    *
-   * @param {DropDownControl} site_control the dropdown used to select
-   * a Townnews site
+   * @param {utl_files.DropDownControl} site_control the dropdown used
+   * to select a Townnews site
    *
-   * @param {DropDownControl} skin_control the dropdown used to select
-   * an application skin
+   * @param {utl_files.DropDownControl} skin_control the dropdown used
+   * to select an application skin
    *
    * @param {FilesView} files_view The file listing display object
    *
@@ -354,93 +194,10 @@ $(function () {
        * from API
        */
       this.add_pkg = (pkg) => {
-        this.jst.create_node(this.list_id, pkg_to_string(pkg))
+        this.jst.create_node(this.list_id, utl_files.pkg_to_string(pkg))
       }
     } // TreeViewPackageList
 
-  /**
-   * @summary Convert package spec to a display name.
-   *
-   * @global
-   *
-   * @description Given a package spec JSON object, returns a string
-   * which contains the same information, suitable for display but
-   * easily converted back to the JSON representation. (see {@link
-   * string_to_pkg})
-   *
-   * @param {Object} pkg Package specification (from python
-   * <code>Package.to_dict()</code>)
-   *
-   * @returns {string} String with format
-   * "<var>application</var>::<var>package</var>[*]", where the
-   * presence of "*" indicates the package is certified
-   *
-   */
-  const pkg_to_string = function (pkg) {
-      let full_name = "";
-      if (pkg.app !== "global") {
-        full_name = pkg.app + "::"
-      }
-      full_name += pkg.name
-      if (pkg.is_certified === "y") {
-        full_name += "*"
-      }
-      return full_name
-    } // pkg_to_string()
-
-  /**
-   * @summary Convert a string with a package description to a
-   * (partial) package specification
-   *
-   * @global
-   *
-   * @param {string} pkg_str The package string in format created by
-   * <code>{@link pkg_to_string}()</code>.
-   *
-   * @returns {Object} Object with keys "app", "name", "is_certified"
-   *
-   */
-  const string_to_pkg = function (pkg_str) {
-      let the_app = "global",
-        the_pkg = pkg_str,
-        is_certified = "n";
-      if (pkg_str.includes('::')) {
-        let parts = pkg_str.split('::')
-        the_app = parts[0]
-        the_pkg = parts[1]
-      }
-      if (the_pkg.endsWith('*')) {
-        is_certified = "y";
-        the_pkg = the_pkg.substring(0, the_pkg.length - 1)
-      }
-      return {
-        "app": the_app,
-        "name": the_pkg,
-        "is_certified": is_certified
-      }
-    } // string_to_pkg()
-
-  /**
-   * @description Universal handling and reporting of jstree errors.
-   *
-   * @protected
-   *
-   * @param {Object} info Information about the error
-   * @param {Object} info.data Extra data about the error
-   * @param {string} info.error A code identifying the type of error?
-   * @param {string} info.id The id of the element generating the error
-   * @param {string} info.plugin The plugin where the error occurred.
-   * @param {string} info.reason Human-readable error description.
-   */
-  const handle_jstree_error = function (info) {
-    console.error("ERROR signaled by jstree: error '" + info.error + "' occurred in element #" +
-      info.id + " (plugin " + info.plugin + " ): " + info.reason)
-    console.log("Extra data associated with the above error:")
-    let parsed = JSON.parse(info.data)
-    for (var key in parsed) {
-      console.log("    " + key + ": " + parsed[key])
-    }
-  };
 
   /**
    * @summary Controls the four package list objects in the main tree
@@ -455,10 +212,54 @@ $(function () {
         BLOCK_LIST = "#pkgs_blocks_list",
         COMP_LIST = "#pkgs_components_list";
 
-      console.log("creating TreeView())")
-      this.site_control = new DropDownControl("#id_site", "#id_site_label", "Site")
-      this.global_control = new DropDownControl("#id_global_skin", "#id_global_skin_label", "Global Skin")
-      this.skin_control = new DropDownControl("#id_app_skin", "#id_app_skin_label", "App Skin")
+      /**
+       * @summary Fill the secondary controls whenever a site is
+       * selected.
+       *
+       * @private
+       *
+       * @description Gets site from site control, calls
+       * <code>api/global_skins_for_site</code> to fill the global
+       * skin control, then <code>api/app_skins_for_site</code> to
+       * fill the skin control.
+       *
+       */
+      const site_control_handler = () => {
+          let domain = this.site_control.text()
+          if (domain !== "certified") {
+            this.global_control.reset("Global Skin")
+            this.global_control.fill_from_api("global_skins_for_site", domain)
+            this.skin_control.reset("App Skin")
+            this.skin_control.fill_from_api("app_skins_for_site", domain)
+            this.reset_nodes()
+          } else {
+            this.global_control.reset("N/A")
+            this.skin_control.reset("N/A")
+            this.make_api_call("certified", "ignored", "ignored", "ignored")
+          }
+        } // site_control_handler()
+
+      /**
+       * @summary Populate the contents of the tree view.
+       *
+       * @description Does nothing unless all three selections have
+       * been made.
+       */
+      const add_pkgs_to_tree = () => {
+          if (!(this.site_control.picked && this.global_control.picked && this.skin_control.picked)) {
+            this.reset_nodes()
+          } else {
+            /* split out application from skin name */
+            let skin_name = this.skin_control.text().split("::"),
+              app_name = skin_name[0];
+            skin_name = skin_name[1];
+            this.make_api_call(this.site_control.text(), this.global_control.text(), app_name, skin_name);
+          }
+        } // add_pkgs_to_tree()
+
+      this.site_control = new utl_files.DropDownControl("#id_site", "#id_site_label", "Site", site_control_handler)
+      this.global_control = new utl_files.DropDownControl("#id_global_skin", "#id_global_skin_label", "Global Skin", add_pkgs_to_tree)
+      this.skin_control = new utl_files.DropDownControl("#id_app_skin", "#id_app_skin_label", "App Skin", add_pkgs_to_tree)
       this.files_view = new FilesView("#files-tree-root")
 
       this.global_node = new TreeViewPackageList(GLOBAL_LIST, this.site_control, this.skin_control)
@@ -490,7 +291,7 @@ $(function () {
        */
       this.onselect_node = (node, selected) => {
           const API_PATH = "/files/api/package_files/"
-          let pkg = string_to_pkg(selected.event.target.textContent),
+          let pkg = utl_files.string_to_pkg(selected.event.target.textContent),
             api_call = "",
             full_name = pkg.name
 
@@ -525,32 +326,6 @@ $(function () {
         } // this.onselect_node()
 
       $(TREE_VIEW).on("select_node.jstree", this.onselect_node)
-
-      /**
-       * @summary Fill the secondary controls whenever a site is
-       * selected.
-       *
-       * @private
-       *
-       * @description Gets site from site control, calls
-       * <code>api/global_skins_for_site</code> to fill the global
-       * skin control, then <code>api/app_skins_for_site</code> to
-       * fill the skin control.
-       *
-       */
-      this.site_control.handler = () => {
-          if (this.site_control.text() !== "certified") {
-            this.global_control.reset("Global Skin")
-            this.global_control.fill_from_api("global_skins_for_site", this.site_control.text())
-            this.skin_control.reset("App Skin")
-            this.skin_control.fill_from_api("app_skins_for_site", this.site_control.text())
-            this.reset_nodes()
-          } else {
-            this.global_control.reset("N/A")
-            this.skin_control.reset("N/A")
-            this.make_api_call("certified", "ignored", "ignored", "ignored")
-          }
-        } // site_control.handler()
 
       /**
        * @summary Get packages based on search controls
@@ -619,40 +394,13 @@ $(function () {
         this.comp_node.reset()
       }
 
-      /**
-       * @summary Populate the contents of the tree view.
-       *
-       * @description Does nothing unless all three selections have
-       * been made.
-       */
-      this.add_pkgs_to_tree = () => {
-          if (!(this.site_control.picked && this.global_control.picked && this.skin_control.picked)) {
-            this.reset_nodes()
-          } else {
-            /* split out application from skin name */
-            let skin_name = this.skin_control.text().split("::"),
-              app_name = skin_name[0];
-            skin_name = skin_name[1];
-            this.make_api_call(this.site_control.text(), this.global_control.text(), app_name, skin_name);
-          }
-        } // add_pkgs_to_tree()
-
-      /**
-       * @summary Fill the Tree View when search form is completed.
-       *
-       * @private
-       */
-      this.global_control.handler = this.skin_control.handler = () => {
-        this.add_pkgs_to_tree()
-      }
-
     } // this.TreeView
 
   //============= immediate code =====================================
   $(TREE_VIEW).jstree({
     core: {
       check_callback: true,
-      error: handle_jstree_error,
+      error: utl_files.handle_jstree_error,
       multiple: false,
       themes: {
         icons: false,
@@ -663,7 +411,8 @@ $(function () {
     //possible: use 'data' to set up automatic ajax call to populate the node
   })
 
-  // technically, so far, we don't have to have TreeView as a constructor
+  // TODO: replace TreeView as constructor with literal object --
+  // never need more than one.
   const the_tree = new TreeView()
 })
 
