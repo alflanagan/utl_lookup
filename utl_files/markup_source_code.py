de63c9d1bff9f5a2b7255d8760ea36e0d1f8a676
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-05-11 17:50:39 -0400

    Remove unused code. Additional tests, multiple bug fixes.

--- utl_files/markup_source_code.py
+++ utl_files/markup_source_code.py
@@ -17,6 +17,7 @@ from html import escape
 from warnings import warn
 from pathlib import Path
 from collections import defaultdict
+from typing import Union, Sequence, Mapping, Any, Iterable
 
 from utl_lib.ast_node import ASTNode, FrozenASTNode
 from utl_lib.handler_ast import UTLParseHandlerAST
@@ -58,86 +59,29 @@ macro custom_uiTabControl();
 end %]
 """
 
+TEST2 = """<div id="blox-right-col" class="grid_2 omega">
+        <img src="global/resources/images/_site/powerup.jpg">
+</div>
+"""
 
-def markup(cssclass, some_string):
-    """Return ``some_string`` wrapped in a <span> element with CSS class == ``cssclass``."""
-    return '<span class="{}">{}</span>'.format(cssclass, some_string)
+TEST3 = ("[%- /* to use the default member-benefits text, "
+         "change the name of this file, or remove it. */ -%]")
 
 
-def interleave(parent_node, subst_kids, source_text):
-    """Returns the result of taking the source_text matching `parent_node` and substituting the
-    text in the array `subst_kids` for the text matching each child of `parent_node`.
+class UTLSourceCode():
+    """Represents a piece of UTL source code, providing lexing, parsing, markup, etc.
 
-    TODO: add long example here
+    :param str source: One or mile lines of UTL source code.
+
     """
-    assert isinstance(parent_node, ASTNode)
-    kids = [node for node in parent_node.children
-            if not (node.attributes["start"] == 0 and node.attributes["end"] == 0)]
-    assert len(kids) == len(subst_kids)
-    first_part = source_text[parent_node.attributes["start"]:
-                             kids[0].attributes["start"]]
-    last_part = source_text[kids[-1].attributes["end"]:
-                            parent_node.attributes["end"]]
-    # marked up text == part before first child + ...
-    results = [first_part]
-    for index in range(len(kids) - 1):
-        # add marked-up text
-        results.append(subst_kids[index])
-        # fill back in any missed chars
-        results.append(source_text[kids[index].attributes["end"]:
-                                   kids[index + 1].attributes["start"]])
-    # don't leave out last marked up item
-    results.append(subst_kids[-1])
-    # and any text after last child
-    results.append(last_part)
-    return "".join(results)
+
+    def __init__(self, source: str) -> None:
+        self.source = source
+        self.ast = None
 
 
-def ast_markup(ast_node: ASTNode, source_text: str):  # pylint: disable=R0912
-    """Return the part of `source_text` matching `ast_node` with syntax markup added."""
-    kids = [ast_markup(kid, source_text) for kid in ast_node.children
-            if not (kid.attributes["start"] == 0 and kid.attributes["end"] == 0)]
-    result = ""
-
-    if ast_node.symbol == 'utl_doc':
-        result = markup("utl-doc", interleave(ast_node, kids, source_text))
-    elif ast_node.symbol == "macro_defn":
-        result = markup("macro-defn", interleave(ast_node, kids, source_text))
-    elif ast_node.symbol == 'document':
-        assert len(kids) == 0
-        result = markup('html_doc', escape(ast_node.attributes["text"]))
-    elif ast_node.symbol == 'id':
-        parts = [ast_node.attributes["symbol"]]
-        # if there are children, kids has marked-up items, but we only want markup
-        # around entire ID. so abandon those markups, create new one
-        parts.extend([node.attributes["symbol"] for node in ast_node.children])
-        result = markup('identifier', ".".join(parts))
-    elif ast_node.symbol == "macro_decl":
-        mname = ast_node.attributes["name"]
-        mtext = source_text[ast_node.attributes["start"]:ast_node.attributes["end"]]
-        other_parts = mtext.split(mname)
-        result = markup("macro-decl", other_parts[0] + markup("macro_name", mname) + other_parts[1])
-    elif ast_node.symbol == "statement_list":
-        result = "".join([kid for kid in kids if kid is not None])
-    elif ast_node.symbol == "else":
-        if not kids:
-            result = ""
-        else:
-            result = markup("else", interleave(ast_node, kids, source_text))
-    elif ast_node.symbol == "elseif_stmts":
-        if not kids:
-            result = ""
-        else:
-            result = markup("elseif", interleave(ast_node, kids, source_text))
-    elif ast_node.symbol == "if":
-        result = markup("if_stmt", interleave(ast_node, kids, source_text))
-    else:
-        warn("AST symbol not recognized: " + ast_node.symbol)
-        result = ""
-    return result
-
-
-def merge_mappings_to_lists(dest, source):
+def merge_mappings_to_lists(dest: Mapping[Any, Sequence],
+                            source: Mapping[Any, Iterable]) -> Mapping[Any, Sequence]:
     """If `dest` and `source` are mappings from a key to a list, adds each item in each list in
     `source` to the list indexed by the same key in `dest`.
 
@@ -149,7 +93,9 @@ def merge_mappings_to_lists(dest, source):
     return dest
 
 
-def ast_markup_struct(ast_node):
+def ast_markup_struct(ast_node: ASTNode) -> Sequence[Mapping[int, Sequence[ASTNode]],
+                                                     Mapping[int, Sequence[ASTNode]],
+                                                     Mapping[int, str]]:
     """Walks tree rooted on `ast_node`. Returns a tuple containing:
     1. a dictionary mapping starting character offsets to AST nodes, and
     2. a ditionary mapping ending character offsets to AST nodes.
@@ -164,7 +110,7 @@ def ast_markup_struct(ast_node):
         documents[ast_node.attributes["start"]] = ast_node.attributes["text"]
     else:
         # omit productions that didn't match any text
-        if not ast_node.attributes["start"] == ast_node.attributes["end"] == 0:
+        if not ast_node.attributes["start"] == ast_node.attributes["end"]:
             start_pos[ast_node.attributes["start"]].append(ast_node)
             end_pos[ast_node.attributes["end"]].append(ast_node)
         for child in ast_node.children:
@@ -178,7 +124,32 @@ def ast_markup_struct(ast_node):
     return (start_pos, end_pos, documents)
 
 
-def get_tags(position, starts, ends):
+# The actual text used for markup is localized to these two procedures, so we can easily change
+# to another method
+def markup_start(node: Union[ASTNode, str]) -> str:
+    """Returns a string used to start markup for text matched by the production `node`.
+
+    :param ASTNode or str node: A node in an AST tree produced from UTL parse
+
+    """
+    if isinstance(node, ASTNode):
+        node = node.symbol
+    return '<span class="{}">'.format(node)
+
+
+def markup_end(node: Union[ASTNode, str]) -> str:
+    """Returns a string suitable for ending markup for text matched by the production `node`.
+
+    """
+    if isinstance(node, ASTNode):
+        node = node.symbol
+    # comment after </span> useful for debugging
+    return '</span><!-- {} -->'.format(node)
+
+
+def get_tags(position: int,
+             starts: Mapping[int, Sequence[ASTNode]],
+             ends: Mapping[int, Sequence[ASTNode]]) -> str:
     """Returns open and/or closing tags for character position `position`.
 
     :param int position: A character position in the source UTL.
@@ -187,33 +158,35 @@ def get_tags(position, starts, ends):
     result = ""
     if position in ends:
         for node in ends[position]:
-            result += '</span><!-- {} -->'.format(node.symbol)
+            result += markup_end(node)
     if position in starts:
         for node in starts[position]:
-            result += '<span class="{}">'.format(node.symbol)
+            result += markup_start(node)
     return result
 
 
-def build_markup(source_text, starts, ends, documents, comment_spans):
+def build_markup(source_text: str,
+                 starts: Mapping[int, Sequence[ASTNode]],
+                 ends: Mapping[int, Sequence[ASTNode]],
+                 documents: Mapping[int, str],
+                 comment_spans: Sequence[Sequence[int]]) -> str:
     """Insert markup into `source_text`, using dictionaries keyed by character position.
 
-    :param str source_text: The original text of the parsed program.
+    :param source_text: The original text of the parsed program.
 
-    :param dict starts: A mapping from character position to a list of AST nodes that begin at
-        that position.
+    :param starts: character position --> list of AST nodes that begin at that position.
 
-    :param dict ends: A mapping from character position to a list of AST nodes that end at that
-        position.
+    :param ends: character position --> list of AST nodes that end at that position.
 
-    :param dict documents: A mapping from character position to a segment of text.
+    :param documents: character position --> a segment of text from `source_text`
 
-    :param iterable comment_spans: A sequence of two-tuples `(start, end)` defining text in
-        source_text as comment text (must be separate as they don't get AST nodes).
+    :param comment_spans: two-tuples `(start, end)` defining text in `source_text` as comment
+        text (note comments have no nodes in AST)
 
     """
     output = ''
     pos = 0
-    end = len(source_text) - 1
+    end = len(source_text)
     com_start_set = set([span[0] for span in comment_spans])
     com_end_set = set([span[1] for span in comment_spans])
 
@@ -224,22 +197,23 @@ def build_markup(source_text, starts, ends, documents, comment_spans):
         if pos in documents:
             output += '<span class="document">{}</span>'.format(escape(documents[pos]))
             pos += len(documents[pos])
+            # if last item is document we're at end + 1
+            pos = pos if pos <= end else end
             continue
         if pos in com_start_set:
             output += '<span class="comment">'
         output += source_text[pos]
         pos += 1
     assert pos == end
-    max_end = max([key for key in ends])
-    for i in range(pos, max_end+1):
-        for end in ends[i]:
-            output += '</span><!-- {} -->'.format(end.symbol)
-    # ends can be after last character in source
-    # output += '</span>' * len(ends[len(source_text) + 1])
+    if ends:
+        max_end = max([key for key in ends])
+        # end pos may be after last char
+        for i in range(pos, max_end + 1):
+            output += get_tags(i, starts, ends)
     return output
 
 
-def check_spans(some_text):
+def check_spans(some_text: str) -> None:
     """Performs validation on '&lt;span&gt;' tags in `some_text`. Reports open tags with no
     closing tag, or closing tags with no open.
 
@@ -254,7 +228,7 @@ def check_spans(some_text):
         warn("extra {} end tags!".format(end_count - span_count))
 
 
-def get_comments(some_text):
+def get_comments(some_text: str) -> Sequence[tuple]:
     """Use :py:class:`utl_files.UTLLexerComments` to find comments.
 
     :param str some_text: UTL source text to pass to lexical analyzer.
@@ -274,7 +248,7 @@ def get_comments(some_text):
     return tuple(spans)
 
 
-def main(filename=None):
+def main(filename: str=None) -> None:
     """Parse file in `filename` (or `TEST1` if `filename` is ``None``) and print the contents
     with syntax markup inserted.
 
@@ -283,7 +257,7 @@ def main(filename=None):
         with Path(filename).open('r') as srcin:
             source_text = srcin.read()
     else:
-        source_text = TEST1
+        source_text = TEST2
 
     handlers = [UTLParseHandlerAST()]
 
@@ -313,11 +287,16 @@ def main(filename=None):
 #
     check_spans(the_markup)
 #
-    with Path('test_data/markup/testmarkup.html').open('r') as template_in:
-        template = template_in.read()
-#
-    print(template.format(the_markup))
+#     with Path('test_data/markup/testmarkup.html').open('r') as template_in:
+#         template = template_in.read()
+
+#     print(template.format(the_markup))
 
 
 if __name__ == '__main__':
-    main(None if len(sys.argv) < 2 else sys.argv[1])
+    try:
+        main(None if len(sys.argv) < 2 else sys.argv[1])
+    except Exception:
+        if len(sys.argv) > 1:
+            sys.stderr.write("Error processing file {}.".format(sys.argv[1]))
+        raise
