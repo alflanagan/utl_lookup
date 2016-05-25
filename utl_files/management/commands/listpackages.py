ec5282a13c0e280fd9a46935e9f06b8daca0b8ba
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-05-25 15:30:24 -0400

    Add CLI param to filter packages, improved output

--- utl_files/management/commands/listpackages.py
+++ utl_files/management/commands/listpackages.py
@@ -11,21 +11,54 @@ Command to list the Townnews packages stored in the database.
 
 """
 
-from django.core.management.base import BaseCommand
-from utl_files.models import Package
+from django.core.management.base import BaseCommand, CommandParser
+from utl_files.models import Package, PackageProp
 
 
 class Command(BaseCommand):
     """Command to list the Townnews packages stored in the database."""
     help = 'Lists all Townnews packages currently known. Warning: list may be long.'
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument('pkg_name', help='OfficeTownnews package')
+    PKG_TYPE_LOOKUP = {"global": Package.GLOBAL_SKIN,
+                       "skin": Package.SKIN,
+                       "block": Package.BLOCK,
+                       "component": Package.COMPONENT}
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)  # not strictly necessary, but...
+        isinstance(parser, CommandParser)
+        # type_choices = list(self.PKG_TYPE_LOOKUP.values()).append("all")
+        # aggravatingly, add_argument() doesn't show dynamic list in help
+        # output
+        parser.add_argument('pkg_type', help='Package type (default = all)',
+                            default='all', nargs='?',
+                            choices=["global", "skin", "block", "component", "all"])
 
     def handle(self, *args, **options):
-        for pkg in Package.objects.order_by("site", "name").all():
+        if 'pkg_type' not in options or options['pkg_type'] == "all":
+            pkgs = Package.objects.all()
+        else:
+            pkg_type = self.PKG_TYPE_LOOKUP[options['pkg_type']]
+            pkgs = Package.objects.filter(pkg_type=pkg_type)
+
+        for pkg in pkgs.order_by("site", "name"):
             isinstance(pkg, Package)
+            try:
+                title = pkg.packageprop_set.get(key="title").value
+            except PackageProp.DoesNotExist:  # pylint: disable=E1101
+                title = pkg.name
             if pkg.site is not None:
-                print("{} -- {}: {}".format(pkg.site.domain, pkg.name, pkg.version))
+                if title == pkg.name:
+                    print("{} - {} {}".format(pkg.site.domain,
+                                                     pkg.name,
+                                                     pkg.version))
+                else:
+                    print("{} - '{}' ({}) {}".format(pkg.site.domain,
+                                                     title, pkg.name,
+                                                     pkg.version))
             else:
-                print("certified -- {}: {}".format(pkg.name, pkg.version))
+                if title == pkg.name:
+                    print("certified - {} {}".format(pkg.name, pkg.version))
+                else:
+                    print("certified - '{}' ({}) {}".format(title, pkg.name,
+                                                            pkg.version))
