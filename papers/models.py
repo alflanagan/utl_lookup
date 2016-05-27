5d06f972ee2ca4cc833ffa20ca5c2cf08afa1caa
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-05-27 14:44:04 -0400

    Added model TownnewsSiteMetaData

--- papers/models.py
+++ papers/models.py
@@ -1,6 +1,11 @@
 "Models for objects related to a particular newspaper."
-from django.db import models
+from pathlib import Path
+import json
+from datetime import datetime
 
+from django.db import models
+from django.utils import timezone
+from pytz import common_timezones
 # pylint: disable=no-member
 
 
@@ -31,6 +36,51 @@ class TownnewsSite(models.Model):
         if self.URL == "http://townnews.com":
             return "certified"
         return self.URL.replace('http://', '')
+
+
+class TownnewsSiteMetaData(models.Model):
+    """Additional information about the site, such as what packages are
+    associated with it (even if they're not in database) and their last
+    download times).
+
+    """
+    class Meta:  # pylint:disable=C0111,R0903
+        # not sure last_download should be in here.
+        unique_together = ['site', 'pkg_name', 'version', 'last_download']
+
+    site = models.ForeignKey('TownnewsSite',
+                             help_text='The site this metadata applies to',
+                             on_delete=models.CASCADE)
+    # we flatten the structure of site_meta.json a bit so we don't deal
+    # w/nested data. If a Package record exists for the package, this data
+    # should be there as well.
+    # fields should be compatible with Package definitions!
+    pkg_name = models.CharField(max_length=250, help_text="TownNews name for this package.")
+    zip_name = models.FilePathField(max_length=2000)
+    version = models.CharField(max_length=20,
+                               help_text="Version number for the package as a whole.")
+    is_certified = models.BooleanField(help_text="Is officially certified/supported by TownNews.")
+    last_download = models.DateTimeField(help_text="When this package's ZIP file was downloaded.")
+
+    @staticmethod
+    def _date_from_str(floatstr):
+        """Utility method to convert float-style string in JSON file to a date."""
+        naive_date = datetime.fromtimestamp(float(floatstr))
+        return timezone.make_aware(naive_date)
+
+    @classmethod
+    def load_file(cls, file_name, site):
+        """Loads site data from a JSON site_meta.json file."""
+        file_name = Path(file_name) if not isinstance(file_name, Path) else file_name
+        with file_name.open('r') as metain:
+            data = json.load(metain, parse_float=cls._date_from_str)
+        for key in data:
+            rcd = data[key]
+            new_md = cls(site=site, pkg_name=key, version=rcd["version"],
+                         zip_name=rcd["zip_name"], last_download=rcd["last_download"],
+                         is_certified=True if rcd["certified"] == 'Y'else False)
+            new_md.full_clean()
+            new_md.save()
 
 
 class NewsPaper(models.Model):
--- papers/test_models.py
+++ papers/test_models.py
@@ -10,9 +10,11 @@ Unit tests for :py:mod:`papers.models`.
 
 
 """
+from pathlib import Path
+
 from django.test import TestCase
 
-from papers.models import TownnewsSite, NewsPaper
+from papers.models import TownnewsSite, NewsPaper, TownnewsSiteMetaData
 
 # pylint: disable=no-member
 
@@ -42,6 +44,8 @@ class TownnewsSiteTestCase(TestCase):
         paper.save()
         item1 = TownnewsSite(URL=self.TEST_URL, name=self.TEST_NAME, paper=paper)
         self.assertEqual(item1.domain, 'toaddagger.com')
+        item2 = TownnewsSite(URL='http://townnews.com', name=self.TEST_NAME, paper=paper)
+        self.assertEqual(item2.domain, 'certified')
 
     def test_str(self):
         """Unit test for :py:meth:`~papers.models.TownnewsSite.__str__`."""
@@ -60,3 +64,35 @@ class NewsPaperTestCase(TestCase):
         self.assertGreater(NewsPaper.objects.count(), 20)  # just to make sure we have test data
         for paper in NewsPaper.objects.all():
             self.assertEqual(str(paper), paper.name)
+
+
+class TownnewsSiteMetaDataTestCase(TestCase):
+    """Unit tests for :py:class:`~papers.models.TownnewsSiteMetaData`."""
+    TEST_FILE1 = Path(__file__).parent / 'test_data/dothan_eagle_site_meta.json'
+
+    EXPECTED_KEYS = set(['global-core-dotham', 'adowl-core-base',
+                         'core_base_classifieds_real_estate', 'core_adowl',
+                         'editorial-core-widget', 'custom_monster_widget',
+                         'editorial-core-newsletter', 'global-core-dotham-redesign',
+                         'core_base_eedition', 'editorial-core-wrapper',
+                         'editorial-core-print-directory', 'core_base_calendar',
+                         'core_base_classifieds_vehicles', 'core_advertising',
+                         'editorial-core-discussions', 'editorial-core-mobile',
+                         'core_base_business', 'editorial-core-base', 'core_site_dotham',
+                         'core_base_discussions', 'custom-newsletter-2', 'form-core-base',
+                         'editorial-core-forms', 'custom-newsletter', 'core_base_classifieds',
+                         'core_base_editorial'])
+
+    def test_load(self):
+        """Unit test for :py:meth:`papers.models.TownnewsSiteMetaData.load_file`"""
+        paper, _ = NewsPaper.objects.get_or_create(name='Dothan Eagle')
+        deagle, _ = TownnewsSite.objects.get_or_create(
+            URL='http://dothaneagle.com',
+            name='dothaneagle.com',
+            paper=paper)
+        TownnewsSiteMetaData.load_file(self.TEST_FILE1, site=deagle)
+        self.assertEqual(TownnewsSiteMetaData.objects.filter(site=deagle).count(),
+                         26)
+        self.assertSetEqual(set([metadata.pkg_name for metadata
+                                 in TownnewsSiteMetaData.objects.filter(site=deagle)]),
+                            self.EXPECTED_KEYS)
