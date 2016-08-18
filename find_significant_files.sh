70afc4c22cf0925d78fe817919d7f20c17cd093e
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-08-18 11:33:08 -0400

    use loop to build flags

--- find_significant_files.sh
+++ find_significant_files.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env dash
-find . -name external -prune -o -name data -prune -o -name test_data -prune -o -name .git -prune -o \
-     -name __pycache__ -prune -o -path './static/*' -prune -o -type f -print
+# list all files that aren't derived, copied, external, git internals, etc.
+EXCLUDE_FLAG=""
+for EXCLUDE in external data test_data .git __pycache__ node_modules doc TAGS jsdocs
+do
+    EXCLUDE_FLAG="${EXCLUDE_FLAG} -name ${EXCLUDE} -prune -o"
+done
+
+find . ${EXCLUDE_FLAG} -path './static/*' -o -type f -print
